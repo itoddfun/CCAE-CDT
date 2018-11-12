0fd10e4af8c49ebf7ff745bbdf27e40cb059acbc
RideWindX <ridewindx@163.com>
2018-11-12 17:35:58 +0800

    Update icp.hpp

--- libraries/eosiolib/icp.hpp
+++ libraries/eosiolib/icp.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <eosiolib/eosio.hpp>
+#include <eosiolib/singleton.hpp>
 
 namespace eosio {
 
@@ -18,4 +19,30 @@ namespace eosio {
       bytes receipt_action;
    };
 
+   struct [[eosio::table]] peer_contract {
+      account_name peer = 0;
+
+      uint64_t last_outgoing_packet_seq = 0;
+      uint64_t last_incoming_packet_seq = 0; // to validate
+      uint64_t last_outgoing_receipt_seq = 0;
+      uint64_t last_incoming_receipt_seq = 0; // to validate
+
+      uint64_t last_finalised_outgoing_receipt_seq = 0;
+
+      uint32_t last_incoming_packet_block_num = 0;
+      uint32_t last_incoming_receipt_block_num = 0;
+      uint32_t last_incoming_receiptend_block_num = 0;
+
+      uint32_t max_finished_block_num() const {
+         return std::min({last_incoming_packet_block_num, last_incoming_receipt_block_num, last_incoming_receiptend_block_num});
+      }
+   };
+
+   typedef eosio::singleton<N(peer), peer_contract> peer_singleton;
+
+   uint64_t next_packet_seq(account_name code) {
+      auto peer = peer_singleton(code, code).get_or_default(peer_contract{});
+      eosio_assert(peer.peer, "empty peer icp contract");
+      return peer.last_outgoing_packet_seq + 1;
+   }
 }
